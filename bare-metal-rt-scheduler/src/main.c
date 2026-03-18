6dc62a7614781f297469015acb6075424af9441c
PapaBoris <alexandru_s96@yahoo.com>
2026-03-18 15:21:42 +0200

    Polling method calculations & code

--- bare-metal-rt-scheduler/src/main.c
+++ bare-metal-rt-scheduler/src/main.c
@@ -1,18 +1,19 @@
-#include <Arduino.h>
+#include <avr/io.h>
 
-// put function declarations here:
-int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+void init()
+{
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+int main()
+{
+  init();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  while(1)
+  {
+
+  }
+  return 0;
 }
